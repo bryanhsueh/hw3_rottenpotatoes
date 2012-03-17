dc09b5199d22394f3bf19ae3d5134fa89fedb0fd
saasbook <saasbook@saasbook.(none)>
2012-03-17 02:00:26 -0700

    HW3-P2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.new(:title => movie['title'], :rating => movie['rating'], :release_date => movie['release_date'])
+    Movie.create(:title => movie['title'], :rating => movie['rating'], :release_date => movie['release_date'])
   end
   #assert false, "Unimplmemented"
 end
@@ -26,4 +26,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(",").each do |rating|
+    uncheck ? uncheck(rating) : check(rating)
+  end
+end
+
+Then /I should not see any movie/ do
+  page.all('table#movies tbody tr').count.should == 0
+end
+
+Then /I should see all of the movies/ do
+  page.all('table#movies tbody tr').count.should == Movie.all.length
 end
